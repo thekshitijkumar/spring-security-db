29487c1ff176bcbcff7f6623286c5fe682be47f5
Kshitij Kumar <97kshitij@gmail.com>
2021-03-07 14:04:46 +0530

    Added support for Signup

--- src/main/java/com/example/springsecuritydb/controllers/UserController.java
+++ src/main/java/com/example/springsecuritydb/controllers/UserController.java
@@ -1,13 +1,21 @@
 package com.example.springsecuritydb.controllers;
 
 import com.example.springsecuritydb.models.User;
+import com.example.springsecuritydb.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @GetMapping("/student")
     public String greetUser(@RequestParam String name)
@@ -29,4 +37,15 @@ public class UserController {
         return "Hello user "+name;
     }
 
+
+    @PostMapping("/signup_student")
+    public void signUp(@RequestBody User user)
+    {
+        //gives username,password,authority
+        //convert password to BCrypt
+        //check if username is unique or not
+        String encodedPasswd=passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPasswd);
+        userRepository.save(user);
+    }
 }
